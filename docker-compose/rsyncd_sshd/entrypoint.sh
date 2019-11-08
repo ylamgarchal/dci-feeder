7d591793e28adf6036ea51c072e501f4af5aba50
Yassine Lamgarchal <yassine.lamgarchal@redhat.com>
2019-11-08 02:32:35 +0100

    feeder_worker/feeder_api: add ssh keys
    Enable ssh access from the worker and api
    containers to the rsyncd_sshd container.

--- docker-compose/rsyncd_sshd/entrypoint.sh
+++ docker-compose/rsyncd_sshd/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-chmod 400 /root/.ssh/authorized_keys
-chown root:root /root/.ssh/authorized_keys
-
 exec /usr/sbin/sshd &
 
 exec /usr/bin/rsync --daemon --no-detach --config /opt/dci-feeder/docker-compose/rsyncd_sshd/rsyncd.conf --log-file=/dev/stdout
